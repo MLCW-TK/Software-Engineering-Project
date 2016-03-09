2ecaefac675f137ddea4c460b3c82ddc83bc26e7
mathiasloh <kyd1992@gmail.com>
2016-03-09 09:10:27 +0100

    Added some implementations

--- Enjoy-your-meal-system/src/enjoyyourmeal/ClientUser.java
+++ Enjoy-your-meal-system/src/enjoyyourmeal/ClientUser.java
@@ -1,24 +1,24 @@
 package enjoyyourmeal;
 
-import java.util.Random;
+import java.util.HashMap;
 
-public class ClientUser implements Users{
+public class ClientUser{
 	private String first_name;
 	private String last_name;
 	private String user_name;
 	private String password;
 	private String email;
-	private String address;
+	private HashMap<String, String> contact;
 	boolean receive_updates = false;
 	private String receive_address = email;
 	
-	public ClientUser(String firstname, String lastname, String username, String password, String email, String address){
+	public ClientUser(String firstname, String lastname, String username, String password, String email, HashMap<String, String> contact){
 		this.first_name = firstname;
 		this.last_name = lastname;
 		this.user_name = username;
 		this.password = password;
 		this.email = email;
-		this.address = address;
+		this.contact = contact;
 	}
 	
 	public String getUsername(){
@@ -29,22 +29,46 @@ public class ClientUser implements Users{
 		return this.email;
 	}
 	
+	public HashMap<String, String> getContactHash(){
+		return contact;
+	}
+	
+	public String getContact(){
+		String s = new String();
+		for (String values: this.contact.keySet()){
+			s += values + " ";
+		}
+		return s;
+	}
+	
+	public String getContactValue(String ContactKey){
+		return contact.get(ContactKey);
+	}
+	
 	public void setReceiveUpdates(boolean bool){
 		this.receive_updates = bool;
 	}
 	
+	public void setEmail(String email){
+		this.email = email;
+	}
+	
+	public void setReceiveAddress(String receive_address){
+		this.receive_address = receive_address;
+	}
 	
 	// Equals and hash
 	@Override
 	public boolean equals(Object o){
-		if (o instanceof Users){
+		if (o instanceof ClientUser){
 			ClientUser c1 = (ClientUser) o; 
-			return (c1.getUsername() == this.user_name || c1.getEmail() == this.email);
+			return (c1.getUsername().equals(this.user_name) || c1.getEmail().equals(this.email));
 		}
 		return false;
 	}
 	
+	@Override
 	public int hashCode(){
-		return 41;
+		return 41*((user_name+email).length()+19);
 	}
 }
--- Enjoy-your-meal-system/src/enjoyyourmeal/RestaurantSystem.java
+++ Enjoy-your-meal-system/src/enjoyyourmeal/RestaurantSystem.java
@@ -1,5 +1,6 @@
 package enjoyyourmeal;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -8,18 +9,18 @@ public class RestaurantSystem {
 	String name;
 	boolean registration_phase = true;
 	boolean user_phase = true;
-	Set<Users> user_list;
+	private Set<ClientUser> user_list;
 	
 	public RestaurantSystem(String restaurant_name){
 		this.name = restaurant_name;
-		this.user_list = new HashSet<Users>();
+		this.user_list = new HashSet<ClientUser>();
 	}
 	
 	public String getRestaurantName(){
 		return this.name;
 	}
 	
-	public void addUser(Users newUser){
+	public void addUser(ClientUser newUser){
 		user_list.add(newUser);
 	}
 	
@@ -32,13 +33,20 @@ public class RestaurantSystem {
 	}
 	
 	public void printUsers(){
-		for (Users users:user_list){
-			System.out.println(users);
+		if (!user_list.isEmpty()){
+			for (ClientUser users:user_list){
+				System.out.println(users.getUsername());
+				System.out.println(users.getEmail());
+			}
+		} else {
+			System.out.println("None to print");
 		}
 	}
 	
 	public void register(){
 		Scanner sc = new Scanner(System.in);
+		HashMap<String, String> contact_details = new HashMap<String, String>();
+		
 		System.out.println("Please enter your first name: ");
 		String first_name = sc.nextLine();
 		System.out.println("Please enter your last name: ");
@@ -49,29 +57,95 @@ public class RestaurantSystem {
 		String password = sc.nextLine();
 		System.out.println("Please enter add an email address: (type 'no' to skip)");
 		String email = sc.nextLine();
-		if (email.toUpperCase() == "NO"){email = null;}
+		if (email.equalsIgnoreCase("NO")){email = "";}
 		System.out.println("Please enter a postal address: (type 'no' to skip)");
 		String address = sc.nextLine();
-		if (address.toUpperCase()=="NO"){address=null;}
-		ClientUser newUser = new ClientUser(first_name, last_name, username, password, email, address);
+		if (address.equalsIgnoreCase("NO")){address="";}
+		if (!address.equalsIgnoreCase("")){
+			contact_details.put("Postal address", address);
+		}
+		
+		System.out.println("Would you like to include additional contact details? (yes/no)");
+		String prompt = sc.nextLine();
+		if (prompt.equalsIgnoreCase("YES")){
+			while (true){
+			System.out.println("Please enter a contact field description (type 'none' to return)");
+			String value1 = sc.nextLine();
+			if(value1.equalsIgnoreCase("NONE")){
+				break;
+			}
+			System.out.println("Please enter a contact field value");
+			String value2 = sc.nextLine();
+			contact_details.put(value1, value2);
+			System.out.println("Would you like to include additional contact details? (yes/no)");
+			}
+		}
+		
+		ClientUser newUser = new ClientUser(first_name, last_name, username, password, email, contact_details);
+		System.out.println(newUser.hashCode());
 		System.out.println("Would you like to receive updates? (yes/no)");
 		String response = sc.nextLine();
-		if (response.toUpperCase()=="YES"){
+		if (response.equalsIgnoreCase("YES")){
 			newUser.setReceiveUpdates(true);
+			if (email == ""){
+				if (contact_details.isEmpty()){
+					System.out.println("No contact details found. Please enter your email address");
+					email = sc.nextLine();
+					newUser.setEmail(email);
+					newUser.setReceiveAddress(email);
+				}
+				else{					
+					System.out.println("No email address set. Please choose your specified contact details");
+					System.out.println(newUser.getContact());
+					String ContactKey = sc.nextLine();
+					
+					while (true){
+						if (newUser.getContactHash().containsKey(ContactKey)){
+							newUser.setReceiveAddress(newUser.getContactValue(ContactKey));
+							break;
+						}
+						else{
+							System.out.println("Wrong input selected. Please try again");
+							ContactKey = sc.nextLine();
+						}
+					}
+				}
+			} else {
+				System.out.println("Would you like to change how you would like to receive updates? (yes/no, default = email");
+				String response2 = sc.nextLine();
+				if (response2.equalsIgnoreCase("YES")){
+					System.out.println(newUser.getContact());
+					String ContactKey = sc.nextLine();
+					while (true){
+						if (newUser.getContactHash().containsKey(ContactKey)){
+							newUser.setReceiveAddress(newUser.getContactValue(ContactKey));
+							break;
+						}
+						else{
+							System.out.println("Wrong input selected. Please try again");
+							ContactKey = sc.nextLine();
+						}
+					}
+				}
+			}
 		}
-		System.out.println("Would you like to set which");
-		
 		if (this.user_list.isEmpty()){
 			this.user_list.add(newUser);
 			System.out.println("Registration successful! Please login in the main menu");
 		}
-		
-		else if (user_list.contains(newUser)){
-			System.out.println("User already exist, please retry again, or log in with the main menu");
-		}
 		else{
-			user_list.add(newUser);
-			System.out.println("Registration successful! Please login from the main menu");
+			if (user_list.contains(newUser)){
+				System.out.println("User/Email already exist, please retry again, or log in with the main menu");
+			}
+			else{
+				user_list.add(newUser);
+				System.out.println("Fucked up here");
+			}
+			
 		}
 	}	
+	
+	public void login(String username, String password){
+		
+	}
 }
