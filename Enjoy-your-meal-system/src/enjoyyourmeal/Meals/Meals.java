2504a7d1171e3456d2ed7a12cf4791baa0c66064
xtk923 <xiongtk@gmail.com>
2016-03-12 12:57:20 +0100

    Added attributes to Meals

--- Enjoy-your-meal-system/src/enjoyyourmeal/Meals/Meals.java
+++ Enjoy-your-meal-system/src/enjoyyourmeal/Meals/Meals.java
@@ -1,5 +1,10 @@
 package enjoyyourmeal.Meals;
 
 public interface Meals {
+	double price = 0;
+	double specialPrice = 0;
+	String description = "";
+	boolean specialOffer = false;
+	Ingredient[] ing = new Ingredient[10];
 	
 }
