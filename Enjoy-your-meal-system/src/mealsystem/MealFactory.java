123086e0b5174af747b653bcf9af082697c1c610
xtk923 <xiongtk@gmail.com>
2016-03-20 16:40:08 +0100

    UML

--- Enjoy-your-meal-system/src/mealsystem/MealFactory.java
+++ Enjoy-your-meal-system/src/mealsystem/MealFactory.java
@@ -3,10 +3,15 @@ package mealsystem;
 import ingredients.Ingredient;
 
 public abstract class MealFactory {
-	/*
+	/**
+	 * 
 	 * Requirement stipulated by our system is that information must be complete before the item
 	 * can be created. If certain information is missing (i.e. author name), illegal item_type
 	 * exception would be thrown.
+	 * @param name
+	 * @param description
+	 * @param ingredients
+	 * @return
 	 */
 	public abstract Meal createMeal(String name, String description, Ingredient ...ingredients);
 	public abstract Meal createMeal(String name, String description, double price, Ingredient ...ingredients);
