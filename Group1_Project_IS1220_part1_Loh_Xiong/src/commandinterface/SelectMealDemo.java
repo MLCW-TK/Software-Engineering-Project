f382b6924a8350f54a015c599cde50d35c550e8d
xtk923 <xiongtk@gmail.com>
2016-04-10 14:14:44 +0200

    just to demo

---
+++ Group1_Project_IS1220_part1_Loh_Xiong/src/commandinterface/SelectMealDemo.java
@@ -0,0 +1,20 @@
+public void selectMeal(String criteria){
+    List<AbstractMeal> l = new List<AbstractMeal>;
+    for (AbstractMeal m : ClientConsole.re1.getMeal_list()){
+        switch(criteria){
+        case "AsItIs":
+            if (m.asItIsCount >= m.ModifiedCount && m.asItIsCount >=m.specialOfferCount){
+                l.add(m);
+            }
+            break;
+        case "AsModified":
+            if (m.modifiedCount >= m.asItIsCount && m.modifiedCount >=m.specialOfferCount){
+                l.add(m);
+            }
+        case "SpecialOffer":
+            if (m.specialOfferCount >= m.asItIsCount && m.specialOfferCount >= m.modifiedCount){
+                l.add(m);
+            }
+        }
+    }
+}
